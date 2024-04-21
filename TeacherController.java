c725a2fb63be251a4bd2055e6960c2a4ed8ca0bc
EugeneSh325 <146085071+EugeneSh325@users.noreply.github.com>
2024-04-22 06:48:06 +1000

    Create TeacherController.java

---
+++ TeacherController.java
@@ -0,0 +1,29 @@
+
+public class TeacherController<T extends Teacher> implements iUserController<T> {
+
+    private iUserService<T> userService;
+
+    public TeacherController(iUserService<T> userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void addUser(T user) {
+        userService.addUser(user);
+    }
+
+    @Override
+    public T getUser(int index) {
+        return userService.getUser(index);
+    }
+
+    @Override
+    public List<T> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @Override
+    public void sortUsers(UserComparator<T> comparator) {
+        userService.sortUsers(comparator);
+    }
+}
